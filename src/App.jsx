553ac129c365c08a71bfb5dbedbc32a3c9ac68cb
Sijin Saji <sijinsaji14@gmail.com>
2024-08-07 10:25:19 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,14 @@
-import { Header } from "./components/Header";
-import { Tasks } from "./components/Tasks";
-import { useTasks } from "./components/Hooks/useTasks";
-import ClearTasks from "./components/Header/ClearTasks";
+import React, { Suspense, lazy } from 'react';
 import './styles/global.css';
 
+const LazyComponent = lazy(() => import('./LazyComponent'));
+
 function App() {
-  const { tasks, addTask, deleteTaskById, toggleTaskCompletedById, clearAllTasks, editTaskById } = useTasks();
-
-  const clearAllTasksWithAnimation = () => {
-    const taskElements = document.querySelectorAll('[id^="task-"]');
-
-    setTimeout(() => {
-      clearAllTasks();
-    }, tasks.length * 100 + 500);
-  };
-
   return (
     <>
-      <Header handleAddTask={addTask} clearAllTasks={clearAllTasksWithAnimation} />
-      <ClearTasks onClear={clearAllTasksWithAnimation} disabled={tasks.length === 0} />
-      <Tasks
-        tasks={tasks}
-        onDelete={deleteTaskById}
-        onComplete={toggleTaskCompletedById}
-        onClear={clearAllTasksWithAnimation}
-        onEdit={editTaskById}
-      />
+    <Suspense fallback={<div>Loading...</div>}>
+      <LazyComponent />
+      </Suspense>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vitejs.dev/config/
+// Vite configuration
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    visualizer({ 
+      open: true, // Open the bundle analyzer report in the browser
+      gzipSize: true, 
+      brotliSize: true
+    })
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split vendor libraries into separate chunks
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          // Additional chunking as needed
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000, // Set chunk size warning limit (in KiB)
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src') // Example alias for shorter imports
+    }
+  }
+});
